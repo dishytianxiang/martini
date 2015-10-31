ce36bebb3685c0e44b4d7bd4ff42feae1598ad7d
dishytianxiang <1066844321@qq.com>
2015-10-31 12:37:57 +0800

    update some code

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 )
 
 //https://github.com/go-martini/martini
+//https://github.com/go-martini/martini
 func main() {
 	m := martini.Classic()
-	m.Get("/",functioin(){
+	m.Use(martini.Static("public"))
+	m.Get("/", func() string {
 		return "Hello world!"
 	})
+	m.Get("/hello/:name", func(params martini.Params) string {
+		return "hello " + params["name"]
+	})
+	m.NotFound(func() (int, string) {
+		return 404, "NOT FOUND !!"
+	})
+	m.RunOnAddr(":8000")
 }
---
+++ public/index.js
@@ -0,0 +1,1 @@
+hello nihao i am index.js
